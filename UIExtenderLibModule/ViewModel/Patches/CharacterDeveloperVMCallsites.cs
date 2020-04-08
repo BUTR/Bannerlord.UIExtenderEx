8ef05b3fdd17ace034732d2dc25307929fb9900a
fnzr <5471818+fnzr@users.noreply.github.com>
2020-04-08 11:05:05 -0300

    Create CharacterDeveloperVMCallsites.cs

---
+++ UIExtenderLibModule/ViewModel/Patches/CharacterDeveloperVMCallsites.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+using TaleWorlds.CampaignSystem.ViewModelCollection.CharacterDeveloper;
+
+namespace UIExtenderLibModule.ViewModel.Patches
+{    
+    [HarmonyPatch(typeof(CharacterDeveloperVM), MethodType.Constructor, new[] { typeof(Action) })]
+    public static class CharacterDeveloperVMConstructorCallsite
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> input)
+        {
+            return ViewModelPatchUtil.TranspilerForVMInstantiation(input);
+        }
+    }
+
+    [HarmonyPatch(typeof(CharacterDeveloperVM), "RefreshValues")]
+    public static class CharacterDeveloperVMRefreshCallsite
+    {
+        public static void Prefix()
+        {
+            var component = UIExtenderLibModule.SharedInstance.ViewModelComponent;
+            component.RefreshMixinsForTypes(new[] { typeof(CharacterDeveloperVM), typeof(CharacterVM) });
+        }
+    }
+}
