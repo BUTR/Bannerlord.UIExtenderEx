70389ec7e9a6deb576b5c8b325f0e961565c6890
Tim Beswick <contact.tim.here@gmail.com>
2020-04-13 13:12:17 +0100

    Added callsites for GauntletPartyScreen, which allows registration of PartyVM mixins

---
+++ UIExtenderLibModule/ViewModel/Patches/GauntletPartyScreenCallsites.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using SandBox.GauntletUI;
+using TaleWorlds.Core;
+
+namespace UIExtenderLibModule.ViewModel.Patches
+{
+    //  Harmony cannot resolve the interface methods by iself, so this route has to be used
+    [HarmonyPatch]
+    public static class GauntletPartyScreenOnActivateCallsite
+    {
+        public static MethodBase TargetMethod()
+        {
+            Type type = typeof(GauntletPartyScreen);
+            MethodInfo interfaceMethod = type.GetInterface(nameof(IGameStateListener)).GetMethod("OnActivate");
+            if (interfaceMethod == null) return null;
+
+            InterfaceMapping map = type.GetInterfaceMap(interfaceMethod.DeclaringType ?? throw new NullReferenceException("Cannot find GauntletPartyScreen IGameStateListener.OnActivate method"));
+            return map.TargetMethods[Array.IndexOf(map.InterfaceMethods, interfaceMethod)];
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> input) => ViewModelPatchUtil.TranspilerForVMInstantiation(input);
+    }
+}
