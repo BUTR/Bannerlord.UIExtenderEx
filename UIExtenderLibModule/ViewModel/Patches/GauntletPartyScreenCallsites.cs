470dd0c9fcb847ebbb48805e73b0b1dedd07c360
Tim Beswick <contact.tim.here@gmail.com>
2020-04-13 23:33:47 +0100

    Added OnInitialize callsite back in

--- UIExtenderLibModule/ViewModel/Patches/GauntletPartyScreenCallsites.cs
+++ UIExtenderLibModule/ViewModel/Patches/GauntletPartyScreenCallsites.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
 using SandBox.GauntletUI;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
 using TaleWorlds.Core;
 
 namespace UIExtenderLibModule.ViewModel.Patches
@@ -22,5 +23,26 @@ namespace UIExtenderLibModule.ViewModel.Patches
         }
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> input) => ViewModelPatchUtil.TranspilerForVMInstantiation(input);
+    }
+
+    // OnInitialize - only runs once each time party screen is opened BUT DOES NOT RUN ON THE FIRST OPEN
+    [HarmonyPatch]
+    public static class GauntletPartyScreenOnInitializeCallsite
+    {
+        public static MethodBase TargetMethod()
+        {
+            Type type = typeof(GauntletPartyScreen);
+            MethodInfo interfaceMethod = type.GetInterface(nameof(IGameStateListener)).GetMethod("OnInitialize");
+            if (interfaceMethod == null) return null;
+    
+            InterfaceMapping map = type.GetInterfaceMap(interfaceMethod.DeclaringType ?? throw new NullReferenceException("Cannot find GauntletPartyScreen IGameStateListener.OnInitialize method"));
+            return map.TargetMethods[Array.IndexOf(map.InterfaceMethods, interfaceMethod)];
+        }
+    
+        public static void Prefix()
+        {
+            ViewModelComponent component = UIExtenderLibModule.SharedInstance.ViewModelComponent;
+            component.RefreshMixinsForTypes(new[] {typeof(PartyVM)});
+        }
     }
 }
