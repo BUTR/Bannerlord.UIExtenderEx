d6bea2e089642c9f171878c8b19e11f88955facc
Vitaly Mikhailov <personal@aragas.org>
2020-11-22 17:25:00 +0300

    Made exit optional

--- src/Bannerlord.UIExtenderEx/SubModule.cs
+++ src/Bannerlord.UIExtenderEx/SubModule.cs
@@ -24,6 +24,8 @@ namespace Bannerlord.UIExtenderEx
 Make sure UIExtenderEx is loaded before them!";
         private const string SErrorOfficialModules =
 @"{=F62r44tj2C}The following modules were loaded before UIExtenderEx:";
+        private const string SMessageContinue =
+@"{=eXs6FLm5DP}It's strongly recommended to terminate the game now. Do you wish to terminate it?";
 
         public SubModule()
         {
@@ -65,8 +67,15 @@ Make sure UIExtenderEx is loaded before them!";
 
             if (sb.Length > 0)
             {
-                MessageBox.Show(sb.ToString(), new TextObject(SWarningTitle).ToString(), MessageBoxButtons.OK);
-                Environment.Exit(1);
+                sb.AppendLine();
+                sb.AppendLine(new TextObject(SMessageContinue).ToString());
+
+                switch (MessageBox.Show(sb.ToString(), new TextObject(SWarningTitle).ToString(), MessageBoxButtons.YesNo))
+                {
+                    case DialogResult.Yes:
+                        Environment.Exit(1);
+                        break;
+                }
             }
         }
 
