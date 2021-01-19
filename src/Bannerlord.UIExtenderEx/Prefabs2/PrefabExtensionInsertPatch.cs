d9f2127f40b051c5a68c6b2fdeddf63570bb906c
Jean-Raphael Auclair-Soucy <Arganancer@gmail.com>
2021-01-19 15:53:27 -0500

    Started working on second API for Prefabs.

---
+++ src/Bannerlord.UIExtenderEx/Prefabs2/PrefabExtensionInsertPatch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Bannerlord.UIExtenderEx.Attributes;
+
+namespace Bannerlord.UIExtenderEx.Prefabs2
+{
+	/// <summary>
+	/// Patch that inserts node(s) relative to the target node specified in the <see cref="PrefabExtensionAttribute.XPath"/> property.
+	/// </summary>
+	public abstract class PrefabExtensionInsertPatch
+	{
+		/// <summary>
+		/// InsertType specifies the placement of the nodes fetched in <see cref="GetPrefabNodes"/> relative to the target node
+		/// specified in the <see cref="PrefabExtensionAttribute.XPath"/> property.<br/><br/>
+		/// <list type="bullet">
+		/// <item>
+		///	<term><see cref="InsertType.Prepend"/></term>
+		/// <description><see cref="GetPrefabNodes"/> are placed before the target node at the same height (siblings).</description>
+		/// </item>
+		/// <item>
+		///	<term><see cref="InsertType.Replace"/></term>
+		/// <description>Target node is replaced with <see cref="GetPrefabNodes"/>. The children of the removed node are added as children to the new node.
+		/// If <see cref="GetPrefabNodes"/> contains more than one child, <see cref="Index"/> will be used to specify which new node should inherit the children.</description>
+		/// </item>
+		/// <item>
+		///	<term><see cref="InsertType.ReplaceAll"/></term>
+		/// <description>Target node and all of its children are replaced with <see cref="GetPrefabNodes"/>.</description>
+		/// </item>
+		/// <item>
+		///	<term><see cref="InsertType.Child"/></term>
+		/// <description><see cref="GetPrefabNodes"/> are inserted as children of the target node.
+		/// If the target node has children, <see cref="Index"/> will be used to place the new nodes relative to the other children.</description>
+		/// </item>
+		/// <item>
+		///	<term><see cref="InsertType.Append"/></term>
+		/// <description><see cref="GetPrefabNodes"/> are placed after the target node at the same height (siblings).</description>
+		/// </item>
+		/// </list>
+		/// </summary>
+		public abstract InsertType Type { get; }
+
+		/// <summary>
+		/// Only used when <see cref="Type"/> is set to <see cref="InsertType.Child"/> or <see cref="InsertType.Replace"/>.<br/>
+		/// See <seealso cref="Type"/> for more details.
+		/// </summary>
+		public virtual int Index { get; } = 0;
+
+		/// <summary>
+		/// Nodes will be inserted in the same order that they appear in this list.
+		/// </summary>
+		public abstract IEnumerable<XmlNode> GetPrefabNodes();
+	}
+}
---
+++ src/Bannerlord.UIExtenderEx/Prefabs2/PrefabExtensionSetAttributePatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace Bannerlord.UIExtenderEx.Prefabs2
+{
+    /// <summary>
+    /// Patch that adds or replaces node's attributes specified by XPath with node from prefab extension
+    /// </summary>
+    public abstract class PrefabExtensionSetAttributePatch
+    {
+	    public abstract List<Attribute> Attributes { get; }
+
+        public readonly struct Attribute
+        {
+	        public Attribute( string name, string value )
+	        {
+		        Name = name;
+		        Value = value;
+	        }
+
+	        public string Name { get; }
+            public string Value { get; }
+        }
+
+        internal void ModifyAttributes( XmlElement element )
+        {
+	        foreach ( Attribute attribute in Attributes )
+	        {
+		        element.SetAttribute( attribute.Name, attribute.Value );
+	        }
+        }
+    }
+}
