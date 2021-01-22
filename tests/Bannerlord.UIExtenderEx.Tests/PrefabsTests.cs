8a46e48023f503c830726549064e99843821e2ee
Jean-Raphael Auclair-Soucy <Arganancer@gmail.com>
2021-01-22 15:15:45 -0500

    More styling issues.

--- tests/Bannerlord.UIExtenderEx.Tests/PrefabsTests.cs
+++ tests/Bannerlord.UIExtenderEx.Tests/PrefabsTests.cs
@@ -1,58 +1,60 @@
-using NUnit.Framework;
-
+using System.Collections.Generic;
 using System.Linq;
 
+using NUnit.Framework;
+
 using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.GauntletUI.PrefabSystem;
 
 namespace Bannerlord.UIExtenderEx.Tests
 {
-	public class PrefabsTests : BaseTests
-	{
-		private const int Elements = 6;
+    public class PrefabsTests : BaseTests
+    {
+        private const int Elements = 6;
 
-		[Test]
-		public void PrefabsInsertTest()
-		{
-			var uiExtender = new UIExtender("TestModule");
-			uiExtender.Register(typeof(PrefabsTests).Assembly);
-			uiExtender.Enable();
+        [Test]
+        public void PrefabsInsertTest()
+        {
+            var uiExtender = new UIExtender("TestModule");
+            uiExtender.Register(typeof(PrefabsTests).Assembly);
+            uiExtender.Enable();
 
-			var widgetTemplateInsert = UIResourceManager.WidgetFactory.GetCustomType("Insert").RootTemplate;
-			var childrenInsert1 = GetChildren(widgetTemplateInsert);
-			var childrenInsert2 = GetChildren(childrenInsert1[0]);
-			var childrenInsert3 = GetChildren(childrenInsert2[0]);
-			Assert.True(childrenInsert3.Count == Elements + 1);
-			Assert.True(childrenInsert3[4].Id == "Insert");
+            var widgetTemplateInsert = UIResourceManager.WidgetFactory.GetCustomType("Insert").RootTemplate;
+            List<WidgetTemplate>? childrenInsert1 = GetChildren(widgetTemplateInsert);
+            List<WidgetTemplate>? childrenInsert2 = GetChildren(childrenInsert1[0]);
+            List<WidgetTemplate>? childrenInsert3 = GetChildren(childrenInsert2[0]);
+            Assert.True(childrenInsert3.Count == Elements + 1);
+            Assert.True(childrenInsert3[4].Id == "Insert");
 
-			var widgetTemplateReplace = UIResourceManager.WidgetFactory.GetCustomType("ReplaceKeepChildren").RootTemplate;
-			var childrenReplace1 = GetChildren(widgetTemplateReplace);
-			var childrenReplace2 = GetChildren(childrenReplace1[0]);
-			var childrenReplace3 = GetChildren(childrenReplace2[0]);
-			Assert.True(childrenReplace3.Count == Elements);
-			Assert.True(childrenReplace3[2].Id == "Replaced");
+            var widgetTemplateReplace = UIResourceManager.WidgetFactory.GetCustomType("ReplaceKeepChildren").RootTemplate;
+            List<WidgetTemplate>? childrenReplace1 = GetChildren(widgetTemplateReplace);
+            List<WidgetTemplate>? childrenReplace2 = GetChildren(childrenReplace1[0]);
+            List<WidgetTemplate>? childrenReplace3 = GetChildren(childrenReplace2[0]);
+            Assert.True(childrenReplace3.Count == Elements);
+            Assert.True(childrenReplace3[2].Id == "Replaced");
 
-			var widgetTemplateInsertAsSiblingAppend = UIResourceManager.WidgetFactory.GetCustomType("InsertAsSiblingAppend").RootTemplate;
-			var childrenInsertAsSiblingAppend1 = GetChildren(widgetTemplateInsertAsSiblingAppend);
-			var childrenInsertAsSiblingAppend2 = GetChildren(childrenInsertAsSiblingAppend1[0]);
-			var childrenInsertAsSiblingAppend3 = GetChildren(childrenInsertAsSiblingAppend2[0]);
-			Assert.True(childrenInsertAsSiblingAppend3.Count == Elements + 1);
-			Assert.True(childrenInsertAsSiblingAppend3[1].Id == "InsertAsSiblingAppend");
+            var widgetTemplateInsertAsSiblingAppend = UIResourceManager.WidgetFactory.GetCustomType("InsertAsSiblingAppend").RootTemplate;
+            List<WidgetTemplate>? childrenInsertAsSiblingAppend1 = GetChildren(widgetTemplateInsertAsSiblingAppend);
+            List<WidgetTemplate>? childrenInsertAsSiblingAppend2 = GetChildren(childrenInsertAsSiblingAppend1[0]);
+            List<WidgetTemplate>? childrenInsertAsSiblingAppend3 = GetChildren(childrenInsertAsSiblingAppend2[0]);
+            Assert.True(childrenInsertAsSiblingAppend3.Count == Elements + 1);
+            Assert.True(childrenInsertAsSiblingAppend3[1].Id == "InsertAsSiblingAppend");
 
-			var widgetTemplateInsertAsSiblingPrepend = UIResourceManager.WidgetFactory.GetCustomType("InsertAsSiblingPrepend").RootTemplate;
-			var childrenInsertAsSiblingPrepend1 = GetChildren(widgetTemplateInsertAsSiblingPrepend);
-			var childrenInsertAsSiblingPrepend2 = GetChildren(childrenInsertAsSiblingPrepend1[0]);
-			var childrenInsertAsSiblingPrepend3 = GetChildren(childrenInsertAsSiblingPrepend2[0]);
-			Assert.True(childrenInsertAsSiblingPrepend3.Count == Elements + 1);
-			Assert.True(childrenInsertAsSiblingPrepend3[0].Id == "InsertAsSiblingPrepend");
+            var widgetTemplateInsertAsSiblingPrepend = UIResourceManager.WidgetFactory.GetCustomType("InsertAsSiblingPrepend").RootTemplate;
+            List<WidgetTemplate>? childrenInsertAsSiblingPrepend1 = GetChildren(widgetTemplateInsertAsSiblingPrepend);
+            List<WidgetTemplate>? childrenInsertAsSiblingPrepend2 = GetChildren(childrenInsertAsSiblingPrepend1[0]);
+            List<WidgetTemplate>? childrenInsertAsSiblingPrepend3 = GetChildren(childrenInsertAsSiblingPrepend2[0]);
+            Assert.True(childrenInsertAsSiblingPrepend3.Count == Elements + 1);
+            Assert.True(childrenInsertAsSiblingPrepend3[0].Id == "InsertAsSiblingPrepend");
 
-			var widgetTemplateSetAttribute = UIResourceManager.WidgetFactory.GetCustomType("SetAttribute").RootTemplate;
-			var childrenSetAttribute1 = GetChildren(widgetTemplateSetAttribute);
-			var childrenSetAttribute2 = GetChildren(childrenSetAttribute1[0]);
-			var childrenSetAttribute3 = GetChildren(childrenSetAttribute2[0]);
-			Assert.True(childrenSetAttribute3.Count == Elements);
-			Assert.True(childrenSetAttribute3[3].AllAttributes.Any(a => a.Key == "CustomAttribute" && a.Value == "Value"));
+            var widgetTemplateSetAttribute = UIResourceManager.WidgetFactory.GetCustomType("SetAttribute").RootTemplate;
+            List<WidgetTemplate>? childrenSetAttribute1 = GetChildren(widgetTemplateSetAttribute);
+            List<WidgetTemplate>? childrenSetAttribute2 = GetChildren(childrenSetAttribute1[0]);
+            List<WidgetTemplate>? childrenSetAttribute3 = GetChildren(childrenSetAttribute2[0]);
+            Assert.True(childrenSetAttribute3.Count == Elements);
+            Assert.True(childrenSetAttribute3[3].AllAttributes.Any(a => a.Key == "CustomAttribute" && a.Value == "Value"));
 
-			uiExtender.Disable();
-		}
-	}
+            uiExtender.Disable();
+        }
+    }
 }
