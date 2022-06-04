1ad873ae3e580c7526d20c5cf8c6c1f661712811
Aragas <personal@aragas.org>
2022-06-04 23:59:28 +0300

    Tests fixes

--- tests/Bannerlord.UIExtenderEx.Tests/Prefabs/TestPrefabExtensionInsertPatch.cs
+++ tests/Bannerlord.UIExtenderEx.Tests/Prefabs/TestPrefabExtensionInsertPatch.cs
@@ -1,12 +1,15 @@
 ﻿using Bannerlord.UIExtenderEx.Attributes;
 using Bannerlord.UIExtenderEx.Prefabs;
 
+using System.Diagnostics.CodeAnalysis;
 using System.Xml;
 
 namespace Bannerlord.UIExtenderEx.Tests.Prefabs
 {
     [PrefabExtension("Insert", "descendant::OptionsScreenWidget[@Id='Options']/Children/Standard.TopPanel/Children/ListPanel/Children")]
+#pragma warning disable CS0618
     internal class TestPrefabExtensionInsertPatch : PrefabExtensionInsertPatch
+#pragma warning restore CS0618
     {
         public override string Id => "Insert";
         public override int Position => 3;
